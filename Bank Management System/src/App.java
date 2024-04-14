1c6d27d59d5fbc74ff2c1cdb8644102fd20e963c
Underemployed <underemployed871@gmail.com>
2024-04-14 18:10:19 +0530

    learned panel

--- Bank Management System/src/App.java
+++ Bank Management System/src/App.java
@@ -1,5 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Database connection details
+        String url = "jdbc:mysql://localhost:3306/";
+        String username = "root";
+        String password = "root";
+
+        // Creating the database
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected to the database!");
+        } catch (SQLException e) {
+            System.out.println("Failed to connect to the database.");
+            e.printStackTrace();
+        }
     }
 }
